d94d9279a14e3656861751d2a57454e848dc40dd
Harismita <harishabcd1122@gmail.com>
2026-07-09 15:08:09 +0530

    Create 180-ConsecutiveNumbers

---
+++ 180-ConsecutiveNumbers/180-ConsecutiveNumbers.sql
@@ -0,0 +1,10 @@
+-- Last updated: 7/9/2026, 3:08:09 PM
+with cte as (
+    select num,
+    lead(num,1) over() num1,
+    lead(num,2) over() num2
+    from logs
+
+)
+
+select distinct num ConsecutiveNums from cte where (num=num1) and (num=num2)
