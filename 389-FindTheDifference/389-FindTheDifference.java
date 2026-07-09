94285f595fab9725777769c414e2ea014a5b58ca
Harismita <harishabcd1122@gmail.com>
2026-07-09 15:07:11 +0530

    Create 389-FindTheDifference

---
+++ 389-FindTheDifference/389-FindTheDifference.java
@@ -0,0 +1,13 @@
+// Last updated: 7/9/2026, 3:07:11 PM
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int sum=0;
+        for(int i=0;i<t.length();i++){
+            sum+=t.charAt(i);
+        }
+        for(int i=0;i<s.length();i++){
+            sum-=s.charAt(i);
+        }
+        return (char)sum;
+    }
+}
