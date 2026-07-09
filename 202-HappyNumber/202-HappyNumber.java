fe448fc5d51634acc73821f09f058e543c2b5593
Harismita <harishabcd1122@gmail.com>
2026-07-09 15:07:48 +0530

    Create 202-HappyNumber

---
+++ 202-HappyNumber/202-HappyNumber.java
@@ -0,0 +1,31 @@
+// Last updated: 7/9/2026, 3:07:48 PM
+import java.util.LinkedList;
+
+class Solution {
+    public boolean isHappy(int n) {
+        
+        int slow = n;
+        int fast = n;
+        do {
+
+            slow = square(slow);
+            fast = square(square(fast));
+        } while (slow != fast);
+
+        return slow == 1;
+    }
+    
+
+    public int square(int num) {
+        
+        int ans = 0;
+        
+        while(num > 0) {
+            int remainder = num % 10;
+            ans += remainder * remainder;
+            num /= 10;
+        }
+        
+        return ans;
+    }
+}
