0c6342071c0458388dd87d87143185697b46588d
Harismita <harishabcd1122@gmail.com>
2026-07-09 15:07:21 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,10 @@
+// Last updated: 7/9/2026, 3:07:20 PM
+class Solution {
+    public boolean canWinNim(int n) {
+        if (n%4==0){
+            return false;
+        }else{
+            return true;
+        }
+    }
+}
