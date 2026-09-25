6cc809f4b1a3a546ed7d7a278f109e1c093a17f1
Harismita <harishabcd1122@gmail.com>
2026-09-25 12:53:43 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -0,0 +1,15 @@
+// Last updated: 9/25/2026, 12:53:43 PM
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) {
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                left++;
+            }
+        }        
+    }
+}
