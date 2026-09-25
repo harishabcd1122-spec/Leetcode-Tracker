2c511eab0240a54ff36f2dd228b8d62069e39a3f
Harismita <harishabcd1122@gmail.com>
2026-09-25 12:54:58 +0530

    Create 204-CountPrimes

---
+++ 204-CountPrimes/204-CountPrimes.java
@@ -0,0 +1,28 @@
+// Last updated: 9/25/2026, 12:54:58 PM
+class Solution {
+    public int countPrimes(int n) {
+        if (n <= 2)
+            return 0;
+        
+        
+        int[] notPrime = new int[n + 1];
+        int count = n - 2;
+        int prime = 2;
+        
+        
+        while (prime * prime < n) {
+            if (notPrime[prime] == 0)
+                for (int num = prime + prime; num < n; num += prime)
+                    if (notPrime[num] == 0) {
+                        count--;
+                        notPrime[num] = 1;
+                    }
+            
+            
+            prime++;
+        }
+        
+        
+        return count;
+    }
+}
