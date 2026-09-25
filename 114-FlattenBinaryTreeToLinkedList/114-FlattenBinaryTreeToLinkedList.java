79cdc8f45934af7c0c681456e352a166ab011b30
Harismita <harishabcd1122@gmail.com>
2026-09-25 12:57:11 +0530

    Create 114-FlattenBinaryTreeToLinkedList

---
+++ 114-FlattenBinaryTreeToLinkedList/114-FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,25 @@
+// Last updated: 9/25/2026, 12:57:11 PM
+class Solution {
+    TreeNode temp = null;
+
+    public void flatten(TreeNode root) {
+        helper(root);
+    }
+    private void helper(TreeNode root){
+        if(root == null) return;
+
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+
+        if(temp != null){
+            temp.right = root;
+            
+        }
+
+        root.left = null;
+        temp = root;
+
+        helper(left);
+        helper(right);
+    }
+}
