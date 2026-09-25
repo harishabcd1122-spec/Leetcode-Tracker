72bc56ee5166f18bb8a9c31f9f4bf6c85358d264
Harismita <harishabcd1122@gmail.com>
2026-09-25 12:53:05 +0530

    Create 383-RansomNote

---
+++ 383-RansomNote/383-RansomNote.java
@@ -0,0 +1,24 @@
+// Last updated: 9/25/2026, 12:53:05 PM
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) {
+            return false;
+        }
+
+        Set<Character> ransomSet = new HashSet<>();
+        for (char c : ransomNote.toCharArray()) {
+            ransomSet.add(c);
+        }
+
+        for (char c : ransomSet) {
+            if (countOccurrences(magazine, c) < countOccurrences(ransomNote, c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int countOccurrences(String str, char c) {
+        return (int) str.chars().filter(ch -> ch == c).count();
+    }
+}
