ad7735d2ba7db3dbc2a90fc902fe527c924ba43c
Harismita <harishabcd1122@gmail.com>
2026-09-25 12:52:08 +0530

    Create 476-NumberComplement

---
+++ 476-NumberComplement/476-NumberComplement.java
@@ -0,0 +1,12 @@
+// Last updated: 9/25/2026, 12:52:08 PM
+class Solution {
+    public int findComplement(int num) {
+        if (num == 0) return 1;
+
+        int bitLength = Integer.toBinaryString(num).length();
+        
+        int mask = (1 << bitLength) - 1;
+        
+        return num ^ mask;
+    }
+}
