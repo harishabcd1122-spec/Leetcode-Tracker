47980bec4f2f9ed1307d56255e48e2cd83736917
Harismita <harishabcd1122@gmail.com>
2026-09-25 12:52:32 +0530

    Create 434-NumberOfSegmentsInAString

---
+++ 434-NumberOfSegmentsInAString/434-NumberOfSegmentsInAString.java
@@ -0,0 +1,17 @@
+// Last updated: 9/25/2026, 12:52:31 PM
+class Solution {
+    public int countSegments(String s) {
+        // Split the string into words using spaces as separators
+        String[] words = s.split(" ");
+        int count = 0;
+
+        // Count non-empty words
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
