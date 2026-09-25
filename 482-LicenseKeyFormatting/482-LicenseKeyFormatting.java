a3a6239194ed67240455c7b2643c14ef24c7b81f
Harismita <harishabcd1122@gmail.com>
2026-09-25 12:52:05 +0530

    Create 482-LicenseKeyFormatting

---
+++ 482-LicenseKeyFormatting/482-LicenseKeyFormatting.java
@@ -0,0 +1,23 @@
+// Last updated: 9/25/2026, 12:52:04 PM
+class Solution {
+    public String licenseKeyFormatting(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+
+        // traverse from end
+        for (int i = s.length() - 1, count = 0; i >= 0; i--) {
+            char c = s.charAt(i);
+
+            if (c == '-') continue;
+
+            if (count == k) {
+                sb.append('-');
+                count = 0;
+            }
+
+            sb.append(Character.toUpperCase(c));
+            count++;
+        }
+
+        return sb.reverse().toString();
+    }
+}
